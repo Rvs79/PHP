3ff0e3f1e375ea488e10c6b226f5e0caae9239aa
Robert <vs.robert4@gmail.com>
2025-08-25 22:28:42 -0300

    primeiro commit

---
+++ poo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <header>
+
+    <?php
+        class Carro {
+            public string $marca;
+            public string $modelo;
+            public string $cor;
+            public int $velocidade = 0;
+
+            function acelerar($valor){
+                $this->velocidade += $valor;
+            }
+
+            function frear($valor){
+                if($this->velocidade <= 0){
+                    echo "Não pode frear a 0km por hora!";
+                } else{
+                    $this->velocidade -= $valor;
+
+                    if($this->velocidade < 0){
+                    $this->velocidade = 0;
+                    }
+                }   
+            }
+        }
+
+        $meuCarro = new Carro;
+
+        $meuCarro->marca = "Volkswagen";
+        $meuCarro->modelo = "Polo";
+        $meuCarro->cor = "Preto";
+        $meuCarro->acelerar(100);
+        $meuCarro->frear(40);
+
+        echo 'Velocidade ' . $meuCarro->velocidade;
+    ?>
+
+    </header>
+</body>
+</html>
